460bc1408584a021e18a7eb6f04a65816442994b
Daniel <daniil.spectr@gmail.com>
2023-02-20 20:30:11 +0200

    add propTypes

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types'; // ES6
 import css from 'components/statistics/Statistics.module.css';
 
 export const Statistics = ({ stats, title }) => {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
@@ -8,7 +14,11 @@ export const Statistics = ({ stats, title }) => {
       <ul className={css.statList}>
         {stats.map(el => {
           return (
-            <li className={css.item} key={el.id}>
+            <li
+              className={css.item}
+              key={el.id}
+              style={{ backgroundColor: `${getRandomHexColor()}` }}
+            >
               <span className={css.label}>{el.label}</span>
               <span className={css.percentage}>{el.percentage}</span>
             </li>
@@ -17,4 +27,15 @@ export const Statistics = ({ stats, title }) => {
       </ul>
     </section>
   );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
 };
