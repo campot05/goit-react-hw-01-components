f260a8ddbe098556228d0e80213373d0d732f4e3
Daniel <daniil.spectr@gmail.com>
2023-02-21 13:46:31 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from '../components/Profile/Profile';
-import { Statistics } from '../components/Statistics/Statistics';
-import { FriendList } from '../components/FriendList/FliendList';
-// import { FriendListItem } from './FriendListItem/FriendListItem';
+import { Profile } from 'components/Profile/Profile';
+// import { Statistics } from 'components/Statistics';
+import { FriendList } from 'components/FriendList/FliendList';
+import { Statistics } from 'components/Statistics/Statistics';
 import user from '../user';
 import data from '../data';
 import friends from '../friends';
